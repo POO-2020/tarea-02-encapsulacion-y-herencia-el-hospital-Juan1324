fdf362f6d0ccec20270725c9cf8b5cdd16f05640
Juan Ramos <jramos32@ucol.mx>
2020-03-10 23:58:56 -0600

    prueba

--- main.js
+++ main.js
@@ -9,19 +9,57 @@ import Hospital from "./hospital.js"
 class Main{
 
     constructor(){
-        
-        this.fecha= new Fecha(2,2,2001)
+        this.paciente1= new Paciente(
+            new Nombre("Samuel","Cardenas","Del Rio"),
+            3121086574,
+            new Fecha(12,10,2010))
         
     }
     probarFecha(){
 
-        console.log("Años "+this.fecha.getAños())
-        console.log("Meses "+this.fecha.getMeses())
-        console.log("Semanas "+this.fecha.getSemanas())
-        console.log("Dias "+this.fecha.getDias())
-        console.log("Dia "+this.fecha.getDia())
-        console.log("Fecha "+this.fecha.getFecha())    
+        let fecha= new Fecha(
+            10,
+            12,
+            2000
+        )
+
+        console.log("Años "+fecha.getAños())
+        console.log("Meses "+fecha.getMeses())
+        console.log("Semanas "+fecha.getSemanas())
+        console.log("Dias "+fecha.getDias())
+        console.log("Dia "+fecha.getDia())
+        console.log("Fecha "+fecha.getFecha())    
+
     }
+    probarTiempo(){
+        let tiempo= new Tiempo(
+            12,
+            30,
+            "am"
+        )
+        console.log(tiempo.getFormato12())
+        console.log(tiempo.getFormato24())
+    }
+
+    probarNombre(){
+        let persona= new Nombre(
+            "Juan",
+            "Ramos",
+            "Campos"
+        )
+        console.log(persona.getNombreCompleto())
+        console.log(persona.getApellidoNombre())
+        console.log(persona.getIniciales())
+    }
+    probarPaciente(){
+        console.log(this.paciente1.getPerfil())
+    }
+
 }
 let app=new Main();
 app.probarFecha();
+app.probarTiempo();
+app.probarNombre();
+app.probarPaciente();
+
+
