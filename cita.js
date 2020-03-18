9774658c690bf6303589c597829195b5d96e4df2
Juan Ramos <jramos32@ucol.mx>
2020-03-17 19:58:37 -0600

    getDoctor&getHora

--- cita.js
+++ cita.js
@@ -5,13 +5,23 @@ import Paciente from "./paciente.js"
 import doctor from "./doctor.js"
 export default class Cita{
     
-    constructor(fecha,hora,doctor,paciente){
+    constructor({fecha,hora,doctor,paciente}){
         this._fecha=fecha;
         this._hora=hora;
         this._doctor=doctor;
         this._paciente=paciente;
     }
-    getCita(){
-        return(`${this._fecha.getFecha()},${this._hora.getFormato24()},${this._doctor.nombre.getNombreCompleto()},${this._paciente.nombre.getNombreCompleto()}`);
+    getDoctor(){
+        return this._doctor
+    }
+    getHora(){
+        return this._hora
+    }
+    getPerfil(){
+        return(`
+        ${this._fecha.getFecha()}
+        ${this._hora.getFormato24()}
+        ${"Dr. "+this._doctor.getDoctor().getApellidoPaterno()}
+        ${this._paciente.getNombre().getNombreCompleto()}`);
     }
 }
