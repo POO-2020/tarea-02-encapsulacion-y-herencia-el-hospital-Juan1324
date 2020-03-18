4a9bbbca9c75e7d8127df0775e52191c9801be31
Juan Ramos <jramos32@ucol.mx>
2020-03-17 22:47:50 -0600

    encapsulado

---
+++ pacienteAsegurado.js
@@ -0,0 +1,23 @@
+import Paciente from "./paciente.js";
+import Fecha from "./fecha.js";
+import Nombre from "./nombre.js";
+
+export default class PacienteAsegurado extends Paciente {
+    constructor({nombre,fecha,telefono,numeroPoliza,finVigencia,compañia}){
+        super({ nombre, fecha, telefono})
+        this._finVigencia= finVigencia;
+        this._numeroPoliza= numeroPoliza;
+        this._finVigencia= numeroPoliza;
+        this._compañia= compañia;
+    }
+    getPerfil(){
+        return(`
+        ${this._nombre.getNombreCompleto()}
+        ${this._fecha.getFecha()}
+        ${this._telefono}
+        ${this._numeroPoliza}
+        ${this._fechaFinVigencia}
+        ${this._compañia}`)
+    }
+
+}
